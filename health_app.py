33bba1a139455b0c547d179992799ba1d7c76e10
ts19131 <114727040+ts19131@users.noreply.github.com>
2022-10-07 19:06:06 +0530

    Update health_app.py

--- health_app.py
+++ health_app.py
@@ -1,209 +1,403 @@
-#importing library
 from tkinter import *
-from tkinter import font
-from PIL import ImageTk, Image 
-import time
+from PIL import ImageTk,Image
+from tkinter import messagebox
+import ast
+import webbrowser
+from twilio.rest import Client
+
+w=Tk()
+w.geometry('900x500')
+w.configure(bg='#262626')#12c4c0')
+w.resizable(0,0)
+w.title('Toggle Menu')
+
+def registration():
+    def main():
+        win = Tk()
+        win.geometry('350x350')
+        win.title('Book an appointment')
+        button = Button(win, text='Book an appointment',command=book)
+        button.place(x=100,y=80)
+        button.config(height=5,width=20)
+        button.pack()
+        win.configure(bg='pink')
+        button2 = Button(win, text='Cancel an appointment',command=cancel)
+        button2.place(x=100,y=200)
+        button2.config(height=5, width=20)
+        button2.pack()
+        button3 = Button(win, text='View an appointment',command=view)
+        button3.place(x=100,y=300)
+        button3.config(height=5, width=20)
+        button3.pack()
+        win.mainloop()
+    
+    def cancel():
+        message = "Appointment has been cancelled"
+    
+    
+    def view():
+        print("view")
+    
+    
+    def message():
+        a = 1
+        account_sid = 'ACb789fb03c14205987d7c88ca0eadf209'
+        auth_token = '803ca069c1bcefb9530d98b5f8a6760f'
+        client = Client(account_sid, auth_token)
+    
+        message = client.messages \
+            .create(
+            body="Your appointment has been booked. Patient Name = {} and Phone Number = {} Your booking number is {}".format(
+                entry1.get(), entry2.get(), a),
+            from_='+18782177081',
+            to='+91{}'.format(entry2.get())
+    
+        )
+        a = a + 1
+    
+    def book():
+        win = Tk()
+        win.geometry('500x500')
+        label1 = Label(win, text='Enter your Name')
+        label1.pack()
+        global entry1
+        entry1 = Entry(win)
+        entry1.pack()
+        label2 = Label(win, text='Enter your Phone Number ')
+        label2.pack()
+        global entry2
+        entry2 = Entry(win)
+        entry2.pack()
+        drop = OptionMenu(win, "Select ", "Fever", "Cold", "Cough", "Headache", "Stomachache", "Others")
+        drop.pack()
+        button = Button(win, text="SUBMIT",command = message)
+        button.pack()
+        # x = drop()
+        # print(x)
+    
+    main()
 
 
-def main_program():
+def advice():
+    w = Tk()
+    w.geometry('250x250')
+    w.title('')
+
+    def open():
+        webbrowser.open('https://main.mohfw.gov.in/department-health-and-family-welfare-directory')
+        
+    button1 = Button(w,width=20,font=('arial',15),text='Advisory',command=open)
+    button1.place(x=10,y=100)
+    w.mainloop()
+    
+
+
+
+def registration2():
     w=Toplevel()
-    w.geometry('900x500')
-    w.configure(bg='#262626')#12c4c0')
-    w.resizable(0,0)
-    w.title('Toggle Menu')
+    w.geometry('925x500')
+    w.title('Login')
+    w.configure(bg='#ff4f5a')
+    w.minsize(925,500)
     
     
-    def default_home():
-        f2=Frame(w,width=900,height=455,bg='#262626')
-        f2.place(x=0,y=45)
-        l2=Label(f2,text='Home',fg='white',bg='#262626')
-        l2.config(font=('Comic Sans MS',90))
-        l2.place(x=290,y=150-45)
-    
-       
-    def home():
-        f1.destroy()
-        f2=Frame(w,width=900,height=455,bg='#262626')
-        f2.place(x=0,y=45)
-        l2=Label(f2,text='Home',fg='white',bg='#262626')
-        l2.config(font=('Comic Sans MS',90))
-        l2.place(x=290,y=150-45)
-        toggle_win()
-     
-    
-    def login():
-        f1.destroy()
-        window = Tk()
-        window.title('SignUp')
-        window.geometry('900x455')
-        window.configure(bg='#fff')
-        window.resizable(False,False)
-        img = PhotoImage(file='login.png')
-        Label(window,image=img,border=0,bg='white').place(x=50,y=90)
+    def signin():
+        signin_win=Frame(w,width=925,height=500,bg='white')
+        signin_win.place(x=0,y=0)
+        f1=Frame(signin_win,width=350,height=350,bg='white')
+        f1.place(x=480,y=100)
         
-        frame = Frame(window,width=350,height=390,bg='red')
-        frame.place(x=480,y=50)
+        global img1
+        img1 = ImageTk.PhotoImage(Image.open("signin.png"))
+        Label(signin_win,image=img1,border=0,bg='white').place(x=50,y=50)
+    
+        l2=Label(signin_win,text="Book an appointment",fg='#ff4f5a',bg='white')
+        l2.config(font=('Microsoft YaHei UI Light',23, 'bold'))
+        l2.place(x=500,y=60)
+    
+        def on_enter(e):
+            e1.delete(0,'end')    
+        def on_leave(e):
+            if e1.get()=='':   
+                e1.insert(0,'Username')
+    
         
-        heading = Label(frame,text='Sign Up',fg='#57a1f8',bg='white',font=("Microsoft Yahei UI Light",23,'bold'))
-        heading.place(x=100,y=5)
+        e1 =Entry(f1,width=25,fg='black',border=0,bg='white')
+        e1.config(font=('Microsoft YaHei UI Light',11, ))
+        e1.bind("<FocusIn>", on_enter)
+        e1.bind("<FocusOut>", on_leave)
+        e1.insert(0,'Username')
+        e1.place(x=30,y=60)    
+        Frame(f1,width=295,height=2,bg='black').place(x=25,y=87)
+    
+        #------------------------------------------------------
+    
+        def on_enter(e):
+            e2.delete(0,'end')    
+        def on_leave(e):
+            if e2.get()=='':   
+                e2.insert(0,'Password')
+    
         
-        # entry
-        user = Entry(frame,width=25,fg='black',border=2,bg='white',font=('Microsoft Yahei UI Light',11))
-        user.place(x=30,y=80)
-        user.insert(0,'Username')
-        Frame(frame,width=295,height=2,bg='black').place(x=25,y=107)
-        toggle_win()
+        e2 =Entry(f1,width=21,fg='black',border=0,bg='white')
+        e2.config(font=('Microsoft YaHei UI Light',11, ))
+        e2.bind("<FocusIn>", on_enter)
+        e2.bind("<FocusOut>", on_leave)
+        e2.insert(0,'Password')
+        e2.place(x=30,y=130)
+    
+        Frame(f1,width=295,height=2,bg='black').place(x=25,y=157)
+    
+        #-mech------------------------------------------------
+        def signin_cmd():
+            
+            file=open('datasheet.txt','r')
+            d=file.read()
+            r=ast.literal_eval(d)
+            file.close()
+            
+    
+            key=e1.get()
+            value=e2.get()
+            
+            if key in r.keys() and value==r[key]:           
+                messagebox.showinfo("","     successfully logged in    ")
+                w.destroy()
+                import main
+            else:
+                messagebox.showwarning('try again', 'invalid username or password')
+    
+    
+        #------------------------------------------------------
+        Button(f1,width=39,pady=7,text='Book',bg='#ff4f5a',fg='white',border=0,command=signin_cmd).place(x=35,y=204)
+        l1=Label(f1,text="Don't have an account?",fg="black",bg='white')
+        l1.config(font=('Microsoft YaHei UI Light',9, ))
+        l1.place(x=75,y=250)
+    
+        b2=Button(f1,width=6,text='Sign up',border=0,bg='white',fg='#ff4f5a',command=signup)
+        b2.place(x=215,y=250)
+    
+    
+    
+    
+    def signup():
+        signup_win=Frame(w,width=925,height=500,bg='white')
+        signup_win.place(x=0,y=0)
+        f1=Frame(signup_win,width=350,height=350,bg='white')
+        f1.place(x=480,y=70)
+    
         
-        
-
-       
-        
-
-    
-    
-    def toggle_win():
-        global f1
-        f1=Frame(w,width=300,height=500,bg='#12c4c0')
-        f1.place(x=0,y=0)
-        
-        #buttons
-        def bttn(x,y,text,bcolor,fcolor,cmd):
-         
-            def on_entera(e):
-                myButton1['background'] = bcolor #ffcc66
-                myButton1['foreground']= '#262626'  #000d33
-    
-            def on_leavea(e):
-                myButton1['background'] = fcolor
-                myButton1['foreground']= '#262626'
-    
-            myButton1 = Button(f1,text=text,
-                           width=42,
-                           height=2,
-                           fg='#262626',
-                           border=0,
-                           bg=fcolor,
-                           activeforeground='#262626',
-                           activebackground=bcolor,            
-                            command=cmd)
-                          
-            myButton1.bind("<Enter>", on_entera)
-            myButton1.bind("<Leave>", on_leavea)
-    
-            myButton1.place(x=x,y=y)
-    
-        bttn(0,80,'H O M E','#0f9d9a','#12c4c0',home)
-        bttn(0,160,'L O G I N / S I G N U P','#0f9d9a','#12c4c0',login)
-        bttn(0,240,'A D V I S O R Y','#0f9d9a','#12c4c0',None)
-        bttn(0,320,'E M E R G E N C Y','#0f9d9a','#12c4c0',None)
-    
-        #
-        def dele():
-            f1.destroy()
-            b2=Button(w,image=img1,
-                   command=toggle_win,
-                   border=0,
-                   bg='#262626',
-                   activebackground='#262626')
-            b2.place(x=5,y=8)
-    
         global img2
-        img2 = ImageTk.PhotoImage(Image.open("close.png"))
+        img2 = ImageTk.PhotoImage(Image.open("signup.png"))
+        Label(signup_win,image=img2,border=0,bg='white').place(x=30,y=90)
     
-        Button(f1,
-               image=img2,
-               border=0,
-               command=dele,
-               bg='#12c4c0',
-               activebackground='#12c4c0').place(x=5,y=10)
+        l2=Label(signup_win,text="Sign up",fg='#ff4f5a',bg='white')
+        l2.config(font=('Microsoft YaHei UI Light',23, 'bold'))
+        l2.place(x=600,y=60)
+    
+        def on_enter(e):
+            e1.delete(0,'end')    
+        def on_leave(e):
+            if e1.get()=='':   
+                e1.insert(0,'Username')
+    
         
+        e1 =Entry(f1,width=25,fg='black',border=0,bg='white')
+        e1.config(font=('Microsoft YaHei UI Light',11, ))
+        e1.bind("<FocusIn>", on_enter)
+        e1.bind("<FocusOut>", on_leave)
+        e1.insert(0,'Username')
+        e1.place(x=30,y=60)
     
-    default_home()
+        Frame(f1,width=295,height=2,bg='black').place(x=25,y=87)
     
-    img1 = ImageTk.PhotoImage(Image.open("open.png"))
+        #------------------------------------------------------
     
-    global b2
-    b2=Button(w,image=img1,
-           command=toggle_win,
-           border=0,
-           bg='#262626',
-           activebackground='#262626')
-    b2.place(x=5,y=8)
+        def on_enter(e):
+            e2.delete(0,'end')    
+        def on_leave(e):
+            if e2.get()=='':   
+                e2.insert(0,'Password')
     
+        
+        e2 =Entry(f1,width=21,fg='black',border=0,bg='white')
+        e2.config(font=('Microsoft YaHei UI Light',11, ))
+        e2.bind("<FocusIn>", on_enter)
+        e2.bind("<FocusOut>", on_leave)
+        e2.insert(0,'Password')
+        e2.place(x=30,y=130)
+    
+        Frame(f1,width=295,height=2,bg='black').place(x=25,y=157)
+    
+        def on_enter(e):
+            e3.delete(0,'end')    
+        def on_leave(e):
+            if e3.get()=='':   
+                e3.insert(0,'Confirm Password')
+    
+        
+        e3 =Entry(f1,width=21,fg='black',border=0,bg='white')
+        e3.config(font=('Microsoft YaHei UI Light',11, ))
+        e3.bind("<FocusIn>", on_enter)
+        e3.bind("<FocusOut>", on_leave)
+        e3.insert(0,'Confirm Password')
+        e3.place(x=30,y=130+70)
+    
+        Frame(f1,width=295,height=2,bg='black').place(x=25,y=157+70)    
+    
+        
+        #Mechenism------------------------------------------------
+        
+        def signup_cmd():
+            key=e1.get()
+            value=e2.get()
+            value2=e3.get()
+            
+            if value==value2:
+                file=open('datasheet.txt','r+')
+                d=file.read()
+                r=ast.literal_eval(d)
+                print(r)
+                
+    
+                dict2={key:value}
+                print(dict2)
+                r.update(dict2)
+                print(r)
+                file.truncate(0)
+                file.close()
+                print(r)
+                file=open('datasheet.txt','w')
+                w=file.write(str(r))
+                 
+                messagebox.showinfo("","     successfully signed up     ")
+                
+            else:
+                messagebox.showwarning('try again', 'password should match ')
+    
+    
+        #-------------------------------------------------------
+        Button(f1,width=39,pady=7,text='Sign up',bg='#ff4f5a',fg='white',border=0,command=signup_cmd).place(x=35,y=204+60)
+        l1=Label(f1,text="Already have an account?",fg="black",bg='white')
+        l1.config(font=('Microsoft YaHei UI Light',9, ))
+        l1.place(x=70,y=250+63)
+    
+        b2=Button(f1,width=6,text='Sign in',border=0,bg='white',fg='#ff4f5a',command=signin)
+        b2.place(x=210,y=250+63)
+    
+    signin() #default screen
     
     w.mainloop()
 
 
-def screen():
 
-    w=Toplevel()
+def default_home():
+    f2=Frame(w,width=900,height=455,bg='#262626')
+    f2.place(x=0,y=45)
+    l2=Label(f2,text='Home',fg='white',bg='#262626')
+    l2.config(font=('Comic Sans MS',90))
+    l2.place(x=290,y=150-45)
+
+   
+def home():
+    f1.destroy()
+    f2=Frame(w,width=900,height=455,bg='#262626')
+    f2.place(x=0,y=45)
+    l2=Label(f2,text='Home',fg='white',bg='white')
+    l2.config(font=('Comic Sans MS',90))
+    l2.place(x=290,y=150-45)
+    toggle_win()
+ 
+
+def appointment():
+    f1.destroy()
+    registration()
+    toggle_win()
+   
     
-    #Using piece of code from old splash screen
-    width_of_window = 427
-    height_of_window = 250
-    screen_width = w.winfo_screenwidth()
-    screen_height = w.winfo_screenheight()
-    x_coordinate = (screen_width/2)-(width_of_window/2)
-    y_coordinate = (screen_height/2)-(height_of_window/2)
-    w.geometry("%dx%d+%d+%d" %(width_of_window,height_of_window,x_coordinate,y_coordinate))
-    #w.configure(bg='#ED1B76')
-    w.overrideredirect(1) #for hiding titlebar
+
+def advisory():
+    f1.destroy()
+    f2=Frame(w,width=900,height=455,bg='white')
+    f2.place(x=0,y=45)
+    l2=Label(f2,text='advisory',fg='black',bg='white')
+    l2.config(font=('Comic Sans MS',90))
+    l2.place(x=320,y=150-45)
+    toggle_win()
     
-    #new window to open
-    def new_win():
-        q=Tk()
-        main_program()
-        q.mainloop()
+
+
+def toggle_win():
+    global f1
+    f1=Frame(w,width=300,height=500,bg='#12c4c0')
+    f1.place(x=0,y=0)
     
-    Frame(w, width=427, height=250, bg='#272727').place(x=0,y=0)
-    label1=Label(w, text='MediCare', fg='white', bg='#272727') #decorate it 
-    label1.configure(font=("Game Of Squids", 24, "bold"))   #You need to install this font in your PC or try another one
-    label1.place(x=80,y=90)
-    
-    label2=Label(w, text='Loading...', fg='white', bg='#272727') #decorate it 
-    label2.configure(font=("Calibri", 11))
-    label2.place(x=10,y=215)
-    
-    #making animation
-    
-    image_a=ImageTk.PhotoImage(Image.open('c2.png'))
-    image_b=ImageTk.PhotoImage(Image.open('c1.png'))
-    
-    
-    
-    
-    for i in range(3): #3loops
-        l1=Label(w, image=image_a, border=0, relief=SUNKEN).place(x=180, y=145)
-        l2=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=200, y=145)
-        l3=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=220, y=145)
-        l4=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=240, y=145)
-        w.update_idletasks()
-        time.sleep(0.5)
-    
-        l1=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=180, y=145)
-        l2=Label(w, image=image_a, border=0, relief=SUNKEN).place(x=200, y=145)
-        l3=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=220, y=145)
-        l4=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=240, y=145)
-        w.update_idletasks()
-        time.sleep(0.5)
-    
-        l1=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=180, y=145)
-        l2=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=200, y=145)
-        l3=Label(w, image=image_a, border=0, relief=SUNKEN).place(x=220, y=145)
-        l4=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=240, y=145)
-        w.update_idletasks()
-        time.sleep(0.5)
-    
-        l1=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=180, y=145)
-        l2=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=200, y=145)
-        l3=Label(w, image=image_b, border=0, relief=SUNKEN).place(x=220, y=145)
-        l4=Label(w, image=image_a, border=0, relief=SUNKEN).place(x=240, y=145)
-        w.update_idletasks()
-        time.sleep(0.5)
+    #buttons
+    def bttn(x,y,text,bcolor,fcolor,cmd):
+     
+        def on_entera(e):
+            myButton1['background'] = bcolor #ffcc66
+            myButton1['foreground']= '#262626'  #000d33
+
+        def on_leavea(e):
+            myButton1['background'] = fcolor
+            myButton1['foreground']= '#262626'
+
+        myButton1 = Button(f1,text=text,
+                       width=42,
+                       height=2,
+                       fg='#262626',
+                       border=0,
+                       bg=fcolor,
+                       activeforeground='#262626',
+                       activebackground=bcolor,            
+                        command=cmd)
+                      
+        myButton1.bind("<Enter>", on_entera)
+        myButton1.bind("<Leave>", on_leavea)
+
+        myButton1.place(x=x,y=y)
+
+    bttn(0,80,'H O M E','#0f9d9a','#12c4c0',home)
+    bttn(0,240,'Goverment Advisory','#0f9d9a','#12c4c0',advice)
+    bttn(0,160,'BOOK AN APPOINTMENT','#0f9d9a','#12c4c0',registration)
+    bttn(0,320,'EMERGENCY','#0f9d9a','#12c4c0',None)
     
     
+
+
+    #
+    def dele():
+        f1.destroy()
+        b2=Button(w,image=img1,
+               command=toggle_win,
+               border=0,
+               bg='#262626',
+               activebackground='#262626')
+        b2.place(x=5,y=8)
+
+    global img2
+    img2 = ImageTk.PhotoImage(Image.open("close.png"))
+
+    Button(f1,
+           image=img2,
+           border=0,
+           command=dele,
+           bg='#12c4c0',
+           activebackground='#12c4c0').place(x=5,y=10)
     
-    w.destroy()
-    new_win()
-    
-screen()
+
+default_home()
+
+img1 = ImageTk.PhotoImage(Image.open("open.png"))
+
+global b2
+b2=Button(w,image=img1,
+       command=toggle_win,
+       border=0,
+       bg='#262626',
+       activebackground='#262626')
+b2.place(x=5,y=8)
+
+
+w.mainloop()
